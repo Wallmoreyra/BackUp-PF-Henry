1e8de86b70d89640515457b6039ccd8f4fa820c7
walter moreyra <49510922+Wallmoreyra@users.noreply.github.com>
2024-03-13 13:49:46 -0300

    comit-02
    crud de usuarios completo, productos también, faltaría quizás algún filtro, y los estilos

---
+++ backend/src/config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const Usuarios = require('../models/Usuarios');
+
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, async (email, password, done) => {
+
+    // Match Email's user
+    const user = await Usuarios.findOne({email})
+    if (!user) {
+        return done(null, false, { message: 'Usuario no encontrado!!'});
+    } else {
+        // Math Password's User
+        const match = await user.matchPassword(password);
+        if (match) {
+            return done(null, user);
+        }else {
+            return done(null, false, {message: 'Contraseña incorrecta!'})
+        }
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await Usuarios.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error, null)
+    }
+    
+});
