746b8e33e580cc6893f28d96edcc84a0ef6a78d3
walter moreyra <49510922+Wallmoreyra@users.noreply.github.com>
2024-03-14 12:52:33 -0300

    deploy
    veremos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
+// require('dotenv').config();
+// const mongoose = require('mongoose')
 
-const app = require('./server');
-require('./database');
+// const app = require('./server');
+// require('./database');
 
-app.listen( app.get('port'), () => {
-    console.log('server on port: ', app.get('port'))
-});
+// app.listen( app.get('port'), () => {
+//     console.log('server on port: ', app.get('port'))
+// });
+
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 9000;
+const app = require('./server')
+
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Conectado a mi base de datos en MongoDB"))
+  .catch((error) => console.log(error));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
