b9dd5487e90fafdbbcb7951d9816f5e42d209407
walter moreyra <49510922+Wallmoreyra@users.noreply.github.com>
2024-03-14 12:36:30 -0300

    asss
    aaaaaa

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-const app = require('./src/server');
-require('./src/database');
+const app = require('./server');
+require('./database');
 
 app.listen( app.get('port'), () => {
     console.log('server on port: ', app.get('port'))
